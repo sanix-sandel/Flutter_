2b420b224aacbe7a37ff55b1fbf31f1e80202938
sanix-sandel <36574404+sanix-sandel@users.noreply.github.com>
2021-06-04 21:37:06 +0300

    basics widgets

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_widgets/home.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Starter Template',
+      theme: ThemeData(
+        primaryColor: Colors.blue,
+      ),
+      home: Home(),
+    );
+  }
+}
