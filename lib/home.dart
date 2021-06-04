cecbc2d664f4e2b5468a040c523e9820df9d2bc3
sanix-sandel <36574404+sanix-sandel@users.noreply.github.com>
2021-06-05 02:00:43 +0300

    Using decorators

--- lib/home.dart
+++ lib/home.dart
@@ -11,24 +11,25 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-          title: Text('Home'),
-          leading: IconButton(
-            icon: Icon(Icons.menu),
-            onPressed: () {},
-          ),
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(Icons.search),
+        appBar: AppBar(
+            title: Text('Home'),
+            leading: IconButton(
+              icon: Icon(Icons.menu),
               onPressed: () {},
             ),
-            IconButton(icon: Icon(Icons.more_vert), onPressed: () {})
-          ],
-          flexibleSpace: SafeArea(
-            child: Icon(Icons.photo_camera, size: 75.0, color: Colors.white70),
-          ),
-          bottom: PopupMenuButtonWidget()),
-      /*PreferredSize(
+            actions: <Widget>[
+              IconButton(
+                icon: Icon(Icons.search),
+                onPressed: () {},
+              ),
+              IconButton(icon: Icon(Icons.more_vert), onPressed: () {})
+            ],
+            flexibleSpace: SafeArea(
+              child:
+                  Icon(Icons.photo_camera, size: 75.0, color: Colors.white70),
+            ),
+            bottom: PopupMenuButtonWidget()),
+        /*PreferredSize(
             child: Container(
                 color: Colors.lightGreen.shade100,
                 height: 75.0,
@@ -38,25 +39,100 @@ class _HomeState extends State<Home> {
                 )),
             preferredSize: Size.fromHeight(75.0),
           )),*/
-      body: Padding(
-          padding: EdgeInsets.all(16.0),
-          child: SafeArea(
-            child: SingleChildScrollView(
-              child: Padding(
-                  padding: EdgeInsets.all(16.0),
-                  child: Column(
-                    children: <Widget>[
-                      const ContainerWithBoxDecorationWidget(),
-                      Divider(),
-                      const ColumnWidget(),
-                      Divider(),
-                      const RowWidget(),
-                      Divider(),
-                      const ColumnAndRowNestingWidget(),
-                    ],
-                  )),
-            ),
-          )),
+        body: SafeArea(
+          child: SingleChildScrollView(
+            child: Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Column(
+                  children: <Widget>[
+                    const ImagesAndIconWidget(),
+                    Divider(),
+                    const BoxDecoratorWidget(),
+                    Divider(),
+                    const InputDecoratorsWidget(),
+                  ],
+                )),
+          ),
+        ));
+  }
+}
+
+class BoxDecoratorWidget extends StatelessWidget {
+  const BoxDecoratorWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(16.0),
+      child: Container(
+        height: 100.0,
+        width: 100.0,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(20.0)),
+            color: Colors.orange,
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey,
+                blurRadius: 10.0,
+                offset: Offset(0.0, 10.0),
+              ),
+            ]),
+      ),
+    );
+  }
+}
+
+class InputDecoratorsWidget extends StatelessWidget {
+  const InputDecoratorsWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: <Widget>[
+      TextField(
+        keyboardType: TextInputType.text,
+        style: TextStyle(color: Colors.grey.shade800, fontSize: 16.0),
+        decoration: InputDecoration(
+          labelText: "Notes",
+          labelStyle: TextStyle(color: Colors.purple),
+          border: OutlineInputBorder(),
+        ),
+      ),
+      Divider(
+        color: Colors.lightGreen,
+        height: 50.0,
+      ),
+      TextFormField(
+        decoration: InputDecoration(labelText: 'Enter your notes'),
+      )
+    ]);
+  }
+}
+
+class ImagesAndIconWidget extends StatelessWidget {
+  const ImagesAndIconWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        Image(
+          image: AssetImage("assets/images/logo.png"),
+          fit: BoxFit.cover,
+          width: MediaQuery.of(context).size.width / 3,
+        ),
+        Image.network(
+          'https://flutter.io/images/catalog­widget­placeholder.png',
+          width: MediaQuery.of(context).size.width / 3,
+        ),
+        Icon(
+          Icons.brush,
+          color: Colors.lightBlue,
+          size: 48.0,
+        ),
+      ],
     );
   }
 }
