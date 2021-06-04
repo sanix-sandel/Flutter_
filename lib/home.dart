eda6788e38bb87eed42c985b5d608a1b80f4060c
sanix-sandel <36574404+sanix-sandel@users.noreply.github.com>
2021-06-04 22:41:31 +0300

    RichText

--- lib/home.dart
+++ lib/home.dart
@@ -83,7 +83,29 @@ class ContainerWithBoxDecorationWidget extends StatelessWidget {
                   offset: Offset(0.0, 10.0))
             ],
           ),
-          child: Center(child: Text('Container'))),
+          child: Center(
+              child: RichText(
+                  text: TextSpan(
+                      text: 'Flutter World',
+                      style: TextStyle(
+                        fontSize: 24.0,
+                        color: Colors.deepPurple,
+                        decoration: TextDecoration.underline,
+                        decorationColor: Colors.deepPurpleAccent,
+                        decorationStyle: TextDecorationStyle.dotted,
+                        fontStyle: FontStyle.italic,
+                        fontWeight: FontWeight.normal,
+                      ),
+                      children: <TextSpan>[
+                TextSpan(text: ' for'),
+                TextSpan(
+                  text: ' Mobile',
+                  style: TextStyle(
+                      color: Colors.deepOrange,
+                      fontStyle: FontStyle.normal,
+                      fontWeight: FontWeight.bold),
+                )
+              ])))),
     ]);
   }
 }
