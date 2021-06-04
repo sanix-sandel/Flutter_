5397fa2bcabe5014aba0ce87d660ea7501f7a50a
sanix-sandel <36574404+sanix-sandel@users.noreply.github.com>
2021-06-05 01:09:44 +0300

    Adding button

--- lib/home.dart
+++ lib/home.dart
@@ -27,7 +27,8 @@ class _HomeState extends State<Home> {
           flexibleSpace: SafeArea(
             child: Icon(Icons.photo_camera, size: 75.0, color: Colors.white70),
           ),
-          bottom: PreferredSize(
+          bottom: PopupMenuButtonWidget()),
+      /*PreferredSize(
             child: Container(
                 color: Colors.lightGreen.shade100,
                 height: 75.0,
@@ -36,7 +37,7 @@ class _HomeState extends State<Home> {
                   child: Text('Bottom'),
                 )),
             preferredSize: Size.fromHeight(75.0),
-          )),
+          )),*/
       body: Padding(
           padding: EdgeInsets.all(16.0),
           child: SafeArea(
@@ -197,4 +198,60 @@ class ColumnAndRowNestingWidget extends StatelessWidget {
       ],
     );
   }
+}
+
+class TodoMenuItem {
+  final String title;
+  final Icon icon;
+  TodoMenuItem({required this.title, required this.icon});
+}
+
+List<TodoMenuItem> foodMenuList = [
+  TodoMenuItem(title: 'Fast Food', icon: Icon(Icons.fastfood)),
+  TodoMenuItem(title: 'Remind Me', icon: Icon(Icons.add_alarm)),
+  TodoMenuItem(title: 'Fast Food', icon: Icon(Icons.flight)),
+  TodoMenuItem(title: 'Music', icon: Icon(Icons.audiotrack)),
+];
+
+class PopupMenuButtonWidget extends StatelessWidget
+    implements PreferredSizeWidget {
+  const PopupMenuButtonWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.lightGreen.shade100,
+      height: preferredSize.height,
+      width: double.infinity,
+      child: Center(
+        child: PopupMenuButton<TodoMenuItem>(
+          icon: Icon(Icons.view_list),
+          onSelected: ((valueSelected) {
+            print('valueSelected: ${valueSelected.title}');
+          }),
+          itemBuilder: (BuildContext context) {
+            return foodMenuList.map((TodoMenuItem todoMenuItem) {
+              return PopupMenuItem<TodoMenuItem>(
+                  value: todoMenuItem,
+                  child: Row(
+                    children: <Widget>[
+                      Icon(todoMenuItem.icon.icon),
+                      Padding(
+                        padding: EdgeInsets.all(8.0),
+                      ),
+                      Text(todoMenuItem.title),
+                    ],
+                  ));
+            }).toList();
+          },
+        ),
+      ),
+    );
+  }
+
+  @override
+  //implement preferredSize
+  Size get preferredSize => Size.fromHeight(75.0);
 }
