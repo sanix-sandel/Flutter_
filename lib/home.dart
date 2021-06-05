d95f35129beab6ab1277aed12aa0703b2895ce09
sanix-sandel <36574404+sanix-sandel@users.noreply.github.com>
2021-06-05 08:59:07 +0300

    Orientation

--- lib/home.dart
+++ lib/home.dart
@@ -8,94 +8,123 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  final GlobalKey<FormState> _formStateKey = GlobalKey<FormState>();
-
-  Order order = Order();
-
-  String _validateItemRequired(String value) {
-    //return "salut";
-    return value.isEmpty ? "Item Required" : "Validated";
-  }
-
-  String _validateItemCount(String value) {
-    int _valueAsInteger = value.isEmpty ? 0 : int.parse(value);
-    return _valueAsInteger == 0 ? 'At least one item is Required' : 'Validated';
-  }
-
-  void _submitOrder() {
-    if (_formStateKey.currentState!.validate()) {
-      _formStateKey.currentState!.save();
-      print('Order item: ${order.item}');
-      print('Order Quantity: ${order.quantity}');
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Home'),
-          leading: IconButton(
-            icon: Icon(Icons.menu),
-            onPressed: () {},
-          ),
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(Icons.search),
-              onPressed: () {},
-            ),
-            IconButton(icon: Icon(Icons.more_vert), onPressed: () {})
-          ],
-          flexibleSpace: SafeArea(
-            child: Icon(Icons.photo_camera, size: 75.0, color: Colors.white70),
-          ),
-        ),
         body: SafeArea(
-          child: SingleChildScrollView(
-            child: Padding(
-                padding: EdgeInsets.all(16.0),
-                child: Column(
-                  children: <Widget>[
-                    Form(
-                      autovalidateMode: AutovalidateMode.always,
-                      key: _formStateKey,
-                      child: Padding(
-                        padding: EdgeInsets.all(16.0),
-                        child: Column(children: <Widget>[
-                          TextFormField(
-                            decoration: InputDecoration(
-                              hintText: 'Espresso',
-                              labelText: 'Item',
-                            ),
-                            validator: (value) => _validateItemRequired(value!),
-                            onSaved: (value) => order.item = value!,
-                          ),
-                          TextFormField(
-                            decoration: InputDecoration(
-                              hintText: '3',
-                              labelText: 'Quantity',
-                            ),
-                            validator: (value) => _validateItemCount(value!),
-                            onSaved: (value) =>
-                                order.quantity = int.parse(value!),
-                          ),
-                          Divider(height: 32.0),
-                          ElevatedButton(
-                            child: Text('Save'),
-                            // color:Colors.lightGreen,
-                            onPressed: () => _submitOrder(),
-                          ),
-                        ]),
-                      ),
-                    ),
-                  ],
-                )),
-          ),
-        ));
+            child: SingleChildScrollView(
+                child: Padding(
+                    padding: EdgeInsets.all(16.0),
+                    child: Column(children: <Widget>[
+                      const OrientationLayoutIconsWidget(),
+                      Divider(),
+                      const OrientationLayoutWidget(),
+                      Divider(),
+                      const GridViewWidget(),
+                      Divider(),
+                      const OrientationBuilderWidget(),
+                    ])))));
   }
 }
 
-class Order {
-  late String item;
-  late int quantity;
+class OrientationLayoutIconsWidget extends StatelessWidget {
+  const OrientationLayoutIconsWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Orientation orientation = MediaQuery.of(context).orientation;
+    return orientation == Orientation.portrait
+        ? Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Icon(Icons.school, size: 48.0),
+            ],
+          )
+        : Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+            //If you turn 90 dg, you get now with a brush :)
+            Icon(
+              Icons.school,
+              size: 48.0,
+            ),
+            Icon(
+              Icons.brush,
+              size: 48.0,
+            )
+          ]);
+  }
+}
+
+class OrientationLayoutWidget extends StatelessWidget {
+  const OrientationLayoutWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Orientation orientation = MediaQuery.of(context).orientation;
+
+    return orientation == Orientation.portrait
+        ? Container(
+            alignment: Alignment.center,
+            color: Colors.yellow,
+            height: 100.0,
+            width: 100.0,
+            child: Text('Portrait'),
+          )
+        : Container(
+            alignment: Alignment.center,
+            color: Colors.lightGreen,
+            height: 100.0,
+            width: 200.0,
+            child: Text('Landscape'),
+          );
+  }
+}
+
+class GridViewWidget extends StatelessWidget {
+  const GridViewWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Orientation orientation = MediaQuery.of(context).orientation;
+
+    return GridView.count(
+      shrinkWrap: true,
+      physics: NeverScrollableScrollPhysics(),
+      crossAxisCount: orientation == Orientation.portrait ? 2 : 4,
+      childAspectRatio: 5.0,
+      children: List.generate(8, (int index) {
+        return Text("Grid $index", textAlign: TextAlign.center);
+      }),
+    );
+  }
+}
+
+class OrientationBuilderWidget extends StatelessWidget {
+  const OrientationBuilderWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext contex) {
+    return OrientationBuilder(
+      builder: (BuildContext context, Orientation orientation) {
+        return orientation == Orientation.portrait
+            ? Container(
+                alignment: Alignment.center,
+                color: Colors.yellow,
+                height: 100.0,
+                width: 100.0,
+                child: Text('Portrait'),
+              )
+            : Container(
+                alignment: Alignment.center,
+                color: Colors.lightGreen,
+                height: 100.0,
+                width: 200.0,
+                child: Text('Landscape'),
+              );
+      },
+    );
+  }
 }
