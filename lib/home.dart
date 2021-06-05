884e112762085335f7965c3ba070d80f8cd058e6
sanix-sandel <36574404+sanix-sandel@users.noreply.github.com>
2021-06-05 19:30:25 +0300

    NavigationBottomBar

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_widgets/fly.dart';
+import 'package:flutter_widgets/birthdays.dart';
+import 'package:flutter_widgets/gratitude.dart';
+import 'package:flutter_widgets/reminders.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -9,24 +11,48 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  int _currentIndex = 0;
+  List _listPages = [];
+  late Widget _currentPage;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _listPages..add(Birthdays())..add(Gratitude)..add(Reminders);
+    print(_listPages);
+    _currentPage = Birthdays();
+  }
+
+  void _changePage(int selectedIndex) {
+    setState(() {
+      print(selectedIndex);
+      _currentIndex = selectedIndex;
+      _currentPage = _listPages[selectedIndex];
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SafeArea(
-            child: Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: GestureDetector(
-          child: Hero(
-              tag: 'format_paint',
-              child: Icon(
-                Icons.format_paint,
-                color: Colors.lightGreen,
-                size: 120.0,
-              )),
-          onTap: () {
-            Navigator.push(
-                context, MaterialPageRoute(builder: (context) => Fly()));
-          }),
-    )));
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: _currentPage,
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.cake), title: Text('Birthdays')),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.sentiment_satisfied), title: Text('Gratitude')),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.access_alarm), title: Text('Reminders')),
+        ],
+        onTap: (selectedIndex) => _changePage(selectedIndex),
+      ),
+    );
   }
 }
