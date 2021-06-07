3d93cdf8e1f8ec4da7a8e4f63fbc0b0bfb6f2fe6
sanix-sandel <36574404+sanix-sandel@users.noreply.github.com>
2021-06-07 11:23:34 +0300

    Custom scroll view

--- lib/home.dart
+++ lib/home.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_widgets/birthdays.dart';
-import 'package:flutter_widgets/gratitude.dart';
-import 'package:flutter_widgets/reminders.dart';
+import 'package:flutter_widgets/widgets/sliver_app_bar.dart';
+import 'package:flutter_widgets/widgets/sliver_list.dart';
+import 'package:flutter_widgets/widgets/sliver_grid.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -11,47 +11,19 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  int _currentIndex = 0;
-  List _listPages = [];
-  late Widget _currentPage;
-
-  @override
-  void initState() {
-    super.initState();
-
-    _listPages..add(Birthdays())..add(Gratitude)..add(Reminders);
-    print(_listPages);
-    _currentPage = Birthdays();
-  }
-
-  void _changePage(int selectedIndex) {
-    setState(() {
-      print(selectedIndex);
-      _currentIndex = selectedIndex;
-      _currentPage = _listPages[selectedIndex];
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: _currentPage,
-        ),
+      appBar: AppBar(
+        title: Text('CustomScrollView - Slivers'),
+        elevation: 0.0,
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _currentIndex,
-        items: [
-          BottomNavigationBarItem(
-              icon: Icon(Icons.cake), title: Text('Birthdays')),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.sentiment_satisfied), title: Text('Gratitude')),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.access_alarm), title: Text('Reminders')),
+      body: CustomScrollView(
+        slivers: <Widget>[
+          const SliverAppBarWidget(),
+          const SliverListWidget(),
+          const SliverGridWidget(),
         ],
-        onTap: (selectedIndex) => _changePage(selectedIndex),
       ),
     );
   }
---
+++ lib/widgets/sliver_app_bar.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class SliverAppBarWidget extends StatelessWidget {
+  const SliverAppBarWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      backgroundColor: Colors.brown,
+      forceElevated: true,
+      expandedHeight: 250.0,
+      flexibleSpace: FlexibleSpaceBar(
+        title: Text(
+          'Parallax Effect',
+        ),
+        background: Image(
+          image: AssetImage('assets/images/tiye.jpg'),
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+}
